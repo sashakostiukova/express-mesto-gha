0dc1e0a14d208de5b47a65de75d24ba66679ef9c
Sasha Kostiukova <sashakostiukova@yandex.ru>
2023-11-29 21:52:51 +0300

    пр14:test14

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
   }),
 }), createUser);
 
