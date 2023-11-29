9c5cfbee6390f653806ef0b2768b6fa76bcaaccd
Sasha Kostiukova <sashakostiukova@yandex.ru>
2023-11-29 22:27:15 +0300

    пр14:linkregexp

--- routes/cards.js
+++ routes/cards.js
@@ -13,7 +13,7 @@ cardRouter.get('/', getCards);
 cardRouter.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required(),
+    link: Joi.string().required().pattern(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/),
   }),
 }), createCard);
 
