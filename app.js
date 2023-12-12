c7f01ae1bb1e1baa023203c223fd4f578ddc48d4
Sasha Kostiukova <sashakostiukova@yandex.ru>
2023-12-12 16:40:32 +0300

    пр15:inprogress

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const { errors } = require('celebrate');
 const { config } = require('./config');
 const { router } = require('./routes');
 const { handleError } = require('./middlewares/handleError');
-
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+// "start": "node app.js",
 const app = express();
 
 mongoose.connect(config.MONGO_URL);
@@ -14,8 +15,12 @@ mongoose.connect(config.MONGO_URL);
 app.use(json());
 app.use(helmet());
 
+app.use(requestLogger); // подключаем логгер запросов
 app.use(router);
+app.use(errorLogger); // подключаем логгер ошибок
 app.use(errors());
 app.use(handleError);
 
-app.listen(config.PORT);
+// app.listen(config.PORT);
+
+module.exports = { app };
